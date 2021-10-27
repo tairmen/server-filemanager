df12acc936e69649be63edab4e6ff4caca387e91
Tair <abduraimovtair@gmail.com>
2021-10-27 13:57:28 +0300

    init code

---
+++ index.js
@@ -0,0 +1,9 @@
+let config = {
+    fsRoot: __dirname + '/uploads',
+    rootName: 'Root folder',
+    port: process.env.PORT || '3020',
+    host: process.env.HOST || 'localhost'
+};
+
+let filemanager = require('@opuscapita/filemanager-server');
+filemanager.server.run(config);
